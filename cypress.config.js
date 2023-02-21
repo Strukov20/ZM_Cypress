0a1ef13f179bdcb0ca226337075917eeacd6449f
Bohdan Strukov <bodya19981997@gmail.com>
2023-02-21 12:10:24 +0200

    add yaml file and fix some tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,21 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://dev-apply.cyberdynemortgage.com/',
+    //change url for other companies, 
+    baseUrl: 'https://qa-apply.cyberdynemortgage.com/',
     viewportHeight : 1080,
     viewportWidth : 1920,
-    defaultCommandTimeout : 20000,
+    defaultCommandTimeout : 30000,
     responseTimeout : 120000,
-    requestTimeout : 30000
+    requestTimeout : 60000,
+    numTestsKeptInMemory : 10
+  },
+  env: {
+    build : "stage",
   },
 });
